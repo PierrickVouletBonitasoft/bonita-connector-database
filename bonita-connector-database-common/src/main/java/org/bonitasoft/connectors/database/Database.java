56c18ca3861aa91bf8efb95504d46c13b2808c0c
Pierrick Voulet <pierrick.voulet@bonitasoft.com>
2015-05-18 18:02:01 -0700

    Add of the HirakiDS implementation in the overall DB Type.

--- bonita-connector-database-common/src/main/java/org/bonitasoft/connectors/database/Database.java
+++ bonita-connector-database-common/src/main/java/org/bonitasoft/connectors/database/Database.java
@@ -14,21 +14,32 @@
  */
 package org.bonitasoft.connectors.database;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
+import javax.naming.Binding;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.bonitasoft.engine.connector.ConnectorException;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 /**
  * @author Matthieu Chaffotte
@@ -41,16 +52,71 @@ public class Database {
 
 	private Context ctx;
 
-	public Database(final String driver, final String url, final String username, final String password) throws ClassNotFoundException, InstantiationException,
-	IllegalAccessException, SQLException {
-		Class.forName(driver);
-		connection = DriverManager.getConnection(url, username, password);
-	}
-
 	public Database(final String dataSource, final Properties properties) throws NamingException, SQLException {
 		ctx = new InitialContext(properties);
 		final DataSource ds = (DataSource) ctx.lookup(dataSource);
 		connection = ds.getConnection();
+	}
+
+	public Database(final String driver, final String url, final String username, final String password) throws ClassNotFoundException, InstantiationException,
+	IllegalAccessException, SQLException, NoSuchAlgorithmException, UnsupportedEncodingException, NamingException {
+		Class.forName(driver);
+		connection = getHikariDataSource(driver, url, username, password).getConnection();
+	}
+
+	private HikariDataSource getHikariDataSource(String driver, String url, String username, String password) throws NamingException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		ctx = getHikariContext();
+		Object object = ctx.lookup(getHashHikariInfo(driver, url, username, password));
+		if(object != null && object instanceof HikariDataSource) {
+			synchronized (Integer.class) {
+				object = ctx.lookup(getHashHikariInfo(driver, url, username, password));
+				if(object != null && object instanceof HikariDataSource) {
+					HikariConfig config = new HikariConfig();
+					config.setJdbcUrl(url);
+					config.setUsername(username);
+					config.setPassword(password);
+					config.addDataSourceProperty("cachePrepStmts", "true");
+					config.addDataSourceProperty("prepStmtCacheSize", "250");
+					config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+					ctx.bind(getHashHikariInfo(driver, url, username, password), new HikariDataSource(config));
+					object = ctx.lookup(getHashHikariInfo(driver, url, username, password));
+				}
+			}
+
+		}
+
+		return (HikariDataSource) object;
+	}
+	
+	static private Context getHikariContext() throws NamingException {
+		Properties properties = new Properties();
+//		properties.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+//		properties.put(Context.PROVIDER_URL, "ldap://localhost:389/dc=etcee,dc=com");
+//		properties.put(Context.SECURITY_PRINCIPAL, "name");
+//		properties.put(Context.SECURITY_CREDENTIALS, "password");
+		properties.put(Context.PROVIDER_URL, "hikari:");
+		return new InitialContext(properties);
+	}
+
+	private String getHashHikariInfo(String driver, String url, String username, String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update((driver + url + username + password).getBytes("UTF-8"));
+		return new String(md.digest());
+	}
+
+	static public List<HikariDataSource> getCurrentHirakiDataSources() throws NamingException {
+		Context ctx = getHikariContext();
+		List<HikariDataSource> list = new ArrayList<HikariDataSource>();
+		NamingEnumeration<Binding> bindings = ctx.listBindings("");
+		while(bindings.hasMore()) {
+			Binding binding = (Binding)bindings.next();
+			if(binding.getObject() instanceof HikariDataSource) {
+				list.add((HikariDataSource) binding.getObject());
+			}
+		}
+		bindings.close();
+
+		return list;
 	}
 
 	public void disconnect() throws SQLException, NamingException {
@@ -64,7 +130,7 @@ public class Database {
 
 	public ResultSet select(final String query) throws SQLException {
 		final Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        return statement.executeQuery(query);
+		return statement.executeQuery(query);
 	}
 
 	public boolean executeCommand(final String command) throws SQLException, ConnectorException {
